95c6b2b5b0e2c6f79011842aaa8e5624acec10c3
Kim Maida <yi.mihi@gmail.com>
2013-12-12 15:35:39 -0500

    Add transclusion to sample directive

--- app/Directives.js
+++ app/Directives.js
@@ -28,6 +28,7 @@ app.directive('directiveName', function() {
 	return {
 		restrict: 'A',
 		templateUrl: 'view/tpl/TemplateTpl.html',
+		transclude: true,
 		link: function(scope, element, attrs) {
 			// watch for async data to become available and update
 			scope.$watch('json', function(json) {
