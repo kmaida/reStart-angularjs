47cf66717d1a00ae61e80c486697aed30688c752
Kim Maida <yi.mihi@gmail.com>
2014-02-19 14:26:02 -0500

    Apply body class in HeaderCtrl.js

--- app/HeaderCtrl.js
+++ app/HeaderCtrl.js
@@ -13,4 +13,19 @@ app.controller('HeaderCtrl', ['$scope', '$location', '$routeParams', 'JSONdata',
 	$scope.navIsActive = function(path) {
 		return $location.path().substr(0, path.length) === path;
 	}
+	
+	$scope.$on('$locationChangeSuccess', function(event, newUrl, oldUrl) {
+		var getBodyClass = function(url) {
+				var bodyClass = url.substr(url.lastIndexOf('/') + 1);
+				
+				return !!bodyClass ? 'page-' + bodyClass : 'page-home';
+			},
+			oldBodyClass = getBodyClass(oldUrl),
+			newBodyClass = getBodyClass(newUrl);
+			
+		angular.element('body')
+			.removeClass(oldBodyClass)
+			.addClass(newBodyClass);
+	});
+	
 }]);
