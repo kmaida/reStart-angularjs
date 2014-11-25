2f40f85c4197c9d8e082ae34f54b439da1a63e9b
Kim Maida <yi.mihi@gmail.com>
2014-11-25 08:38:42 -0500

    * Continue converting to mediaCheck. * Separate header from index. * Update viewSwitch directive to use mediaCheck.

--- src/app/ui/directives/ViewSwitchDir.js
+++ src/app/ui/directives/ViewSwitchDir.js
@@ -1,24 +1,22 @@
-// For template switching based on viewport size - updates as viewport is resized
-// In the view controller, set a default viewformat size based on the screen width onload
-app.directive('viewSwitch', ['GlobalObj', function(GlobalObj) {
+// For events based on viewport size - updates as viewport is resized
+app.directive('viewSwitch', ['mediaCheck', '$timeout', function(mediaCheck, $timeout) {
 	return {
 		restrict: 'A',
 		controller: function($scope) {
-			// get the viewport size onload and store it for use in the view
-			$scope.viewformat = GlobalObj.getviewformat();
-		},
-		link: function(scope, element, attrs) {
-			$(document.body)
-				.on('enter-large', function(e) {
-					scope.$apply(function() {
-						scope.viewformat = 'large';
+			mediaCheck.init({
+				scope: $scope,
+				mq: '(max-width: 640)',
+				enter: function() {
+					$timeout(function() {
+						$scope.viewformat = 'small';
 					});
-				})
-				.on('enter-small', function(e) {
-					scope.$apply(function() {
-						scope.viewformat = 'small';
+				},
+				exit: function() {
+					$timeout(function() {
+						$scope.viewformat = 'large';
 					});
-				});
+				}
+			});
 		}
 	};
 }]);
