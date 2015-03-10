0df8a9f6d9733b91a99d0d879267b9517374f75c
Kim Maida <yi.mihi@gmail.com>
2015-03-10 11:40:48 -0400

    * Single responsibility updates - split out routes, constants

---
+++ src/ng-app/core/mq.constant.js
@@ -0,0 +1,6 @@
+angular.module('myApp')
+// media query constants
+	.constant('MQ', {
+		SMALL: '(max-width: 768px)',
+		LARGE: '(min-width: 769px)'
+	});
---
+++ src/ng-app/core/routes.route.js
@@ -0,0 +1,19 @@
+// routes
+angular.module('myApp')
+	.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'ng-app/home/Home.view.html'
+			})
+			.when('/subpage', {
+				templateUrl: 'ng-app/sub/Sub.view.html'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+		$locationProvider
+			.html5Mode({
+				enabled: true
+			})
+			.hashPrefix('!');
+	}]);
