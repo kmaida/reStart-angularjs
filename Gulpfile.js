6d5d684bd628fe116bdf5df9101d98e9a17052e5
Kim Maida <yi.mihi@gmail.com>
2015-03-20 22:54:38 -0400

    * Gulpfile - add path variables

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,29 +8,32 @@ var gulp = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),
 	concat = require('gulp-concat');
 
-/*
-	Install required Gulp Plugins:
+// Paths
+var basePath = {
+	src: './src',
+	dest: './src'
+};
 
-	The "--save-dev" flag will automatically add it to your package.json.  If the files
-	already exist there, you don't need that flag, and can just run "npm install"
+var path = {
+	css: {
+		src: basePath.src + '/assets/css/scss/',
+		dest: basePath.dest + '/assets/css/'
+	},
+	js: {
+		src: basePath.src + '/assets/js/',
+		dest: basePath.dest + '/assets/js/'
+	},
+	jsLibs: {
+		src: basePath.src + '/assets/js/libs/',
+		dest: basePath.dest + '/assets/js/libs/'
+	},
+	jsAngular: {
+		src: basePath.src + '/ng-app/',
+		dest: basePath.dest + '/ng-app/'
+	}
+};
 
-	To install the above plugins at the command prompt:
-
-	npm install gulp --save-dev
-	npm install gulp-util --save-dev
-	npm install gulp-uglify --save-dev
-	npm install gulp-sass --save-dev
-	npm install gulp-sourcemaps --save-dev
-	npm install gulp-minify-css --save-dev
-	npm install gulp-autoprefixer --save-dev
-	npm install gulp-concat --save-dev
-	npm install gulp-watch --save-dev
-
-	To install all of the above at one time, run the following line at the command prompt:
-	npm install gulp gulp-util gulp-uglify gulp-sass gulp-sourcemaps gulp-minify-css gulp-autoprefixer gulp-concat --save-dev
- */
-
-// Use "gulp --prod" to trigger production/build mode from commandline
+// Run "gulp --prod" to trigger production/build mode
 var isProduction = false;
 
 if (gutil.env.prod) {
@@ -45,7 +48,7 @@ function errorHandler(err){
 
 // Compile SCSS
 gulp.task('styles', function() {
-	return gulp.src('./src/assets/css/scss/styles.scss')
+	return gulp.src(path.css.src + 'styles.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass({ style: 'expanded' }))
 		.pipe(autoprefixer({
@@ -54,45 +57,45 @@ gulp.task('styles', function() {
 		}))
 		.pipe(sourcemaps.write())
 		.pipe(isProduction ? minifyCSS() : gutil.noop() )
-		.pipe(gulp.dest('./src/assets/css/'));
-});
-
-// JS Libs
-gulp.task('jsLibs', function() {
-	return gulp.src(['./src/assets/js/libs/jquery.js', './src/assets/js/libs/angular.js', './src/assets/js/libs/**/*.js', '!./src/assets/js/libs/modernizr.min.js', '!./src/assets/js/libs/libs.js'])
-		.pipe(concat('libs.js'))
-		//.pipe(isProduction ? uglify() : gutil.noop() )	// to unminify libs in dev, uncomment this and comment out the next line instead
-		.pipe(uglify())
-		.pipe(gulp.dest('./src/assets/js/libs/'));
+		.pipe(gulp.dest(path.css.dest));
 });
 
 // JS
 gulp.task('js', function() {
-	return gulp.src(['./src/assets/js/**/*.js', '!./src/assets/js/scripts.js', '!./src/assets/js/libs/*'])
+	return gulp.src([path.js.src + '**/*.js', '!' + path.js.src + 'scripts.js', '!' + path.js.src + 'libs/*'])
 		.pipe(sourcemaps.init())
 		.pipe(concat('scripts.js'))
 		.pipe(sourcemaps.write())
 		.pipe(isProduction ? uglify() : gutil.noop() )
-		.pipe(gulp.dest('./src/assets/js/'));
+		.pipe(gulp.dest(path.js.dest));
+});
+
+// JS Libs
+gulp.task('jsLibs', function() {
+	return gulp.src([path.jsLibs.src + 'jquery.js', path.jsLibs.src + 'angular.js', path.jsLibs.src + '**/*.js', '!' + path.jsLibs.src + 'modernizr.min.js', '!' + path.jsLibs.src + 'libs.js'])
+		.pipe(concat('libs.js'))
+		//.pipe(isProduction ? uglify() : gutil.noop() )	// to unminify libs in dev, uncomment this and comment out the next line instead
+		.pipe(uglify())
+		.pipe(gulp.dest(path.jsLibs.dest));
 });
 
 // AngularJS
 gulp.task('jsAngular', function() {
-	return gulp.src(['./src/ng-app/ngStartup.js', './src/ng-app/**/*.js', '!./src/ng-app/ng-app.js'])
+	return gulp.src([path.jsAngular.src + 'ngStartup.js', path.jsAngular.src + '**/*.js', '!' + path.jsAngular.src + 'ng-app.js'])
 		.pipe(sourcemaps.init())
 		.pipe(concat('ng-app.js'))
 		.pipe(sourcemaps.write())
 		.pipe(isProduction ? uglify() : gutil.noop() )
-		.pipe(gulp.dest('./src/ng-app/'));
+		.pipe(gulp.dest(path.jsAngular.dest));
 });
 
 // default build task
 // use "gulp --prod" to trigger production/build mode from commandline
 gulp.task('default', ['styles', 'jsLibs', 'js', 'jsAngular'], function() {
 	if (!isProduction) {
-		gulp.watch('./src/assets/css/scss/**/*.scss', ['styles']);
-		gulp.watch('./src/assets/js/libs/**/*.js', ['jsLibs']);
-		gulp.watch(['./src/assets/js/**/*.js', '!./src/assets/js/scripts.js', '!./src/assets/js/libs/*'], ['js']);
-		gulp.watch(['./src/ng-app/**/*.js', '!./src/ng-app/ng-app.js'], ['jsAngular']);
+		gulp.watch(path.css.src + '**/*.scss', ['styles']);
+		gulp.watch([path.jsLibs.src + '**/*.js', '!' + path.jsLibs.src + 'libs.js'], ['jsLibs']);
+		gulp.watch([path.js.src + '**/*.js', '!' + path.js.src + 'scripts.js', '!' + path.js.src + 'libs/*'], ['js']);
+		gulp.watch([path.jsAngular.src + '**/*.js', '!' + path.jsAngular.src + 'ng-app.js'], ['jsAngular']);
 	}
 });
