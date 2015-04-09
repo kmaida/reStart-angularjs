6b53eb477f8a4367a2bcb6c9c5ef1792e8a08c79
Kim Maida <yi.mihi@gmail.com>
2015-04-09 16:01:09 -0400

    * Update NavControl directive to use data object and private functions/vars

--- src/ng-app/header/NavControl.dir.js
+++ src/ng-app/header/NavControl.dir.js
@@ -7,70 +7,66 @@
 
 	navControl.$inject = ['mediaCheck', 'MQ', '$timeout'];
 
-	/**
-	 * function navControl()
-	 *
-	 * @param mediaCheck
-	 * @param MQ
-	 * @param $timeout
-	 * @returns {{restrict: string, link: navControlLink}}
-	 */
 	function navControl(mediaCheck, MQ, $timeout) {
 
 		navControlLink.$inject = ['$scope', '$element', '$attrs'];
 
-		/**
-		 * function navControlLink()
-		 *
-		 * @param $scope
-		 * @param $element
-		 * @param $attrs
-		 *
-		 * navControl directive link function
-		 */
-		function navControlLink($scope, $element, $attrs) {
-			var body = angular.element('body'),
-				openNav = function () {
-					body
-						.removeClass('nav-closed')
-						.addClass('nav-open');
-				},
-				closeNav = function () {
-					body
-						.removeClass('nav-open')
-						.addClass('nav-closed');
-				};
+		function navControlLink($scope) {
+			// data object
+			$scope.nav = {};
+
+			var _body = angular.element('body'),
+				_navOpen;
+
+			// open mobile navigation (private)
+			function _openNav() {
+				_body
+					.removeClass('nav-closed')
+					.addClass('nav-open');
+
+				_navOpen = true;
+			}
+
+			// close mobile navigation (private)
+			function _closeNav() {
+				_body
+					.removeClass('nav-open')
+					.addClass('nav-closed');
+
+				_navOpen = false;
+			}
 
 			mediaCheck.init({
 				scope: $scope,
 				mq: MQ.SMALL,
 				enter: function () {
-					closeNav();
+					_closeNav();
 
 					$timeout(function () {
-						$scope.toggleNav = function () {
-							if (body.hasClass('nav-closed')) {
-								openNav();
+						// toggle mobile navigation open/closed
+						$scope.nav.toggleNav = function () {
+							if (!_navOpen) {
+								_openNav();
 							} else {
-								closeNav();
+								_closeNav();
 							}
 						};
 					});
 
-					$scope.$on('$locationChangeSuccess', closeNav);
+					$scope.$on('$locationChangeSuccess', _closeNav);
 				},
 				exit: function () {
 					$timeout(function () {
-						$scope.toggleNav = null;
+						$scope.nav.toggleNav = null;
 					});
 
-					body.removeClass('nav-closed nav-open');
+					_body.removeClass('nav-closed nav-open');
 				}
 			});
 		}
 
 		return {
-			restrict: 'A',
+			restrict: 'EA',
 			link: navControlLink
 		};
 	}
