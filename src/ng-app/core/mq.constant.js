a114f9a2619f8a45181c8a2d661cefc91b110a2c
Kim Maida <yi.mihi@gmail.com>
2015-11-09 11:12:27 -0500

    * Media Query CONSTANTS

--- src/ng-app/core/mq.constant.js
+++ src/ng-app/core/mq.constant.js
@@ -1,12 +1,13 @@
 (function() {
 	'use strict';
 
+	// media query constants
+	var MQ = {
+		SMALL: '(max-width: 767px)',
+		LARGE: '(min-width: 768px)'
+	};
+
 	angular
 		.module('myApp')
-		// media query constants
-		.constant('MQ', {
-			SMALL: '(max-width: 767px)',
-			LARGE: '(min-width: 768px)'
-		});
-
+		.constant('MQ', MQ);
 })();
