d501e2f027ac1e5d09d8a44f140a8634156f4446
Kim Maida <yi.mihi@gmail.com>
2015-11-09 11:18:00 -0500

    * Change JSONData to a factory instead of a service (styleguide)

---
+++ src/ng-app/core/JSONData.factory.js
@@ -0,0 +1,55 @@
+// fetch JSON data to share between controllers
+(function() {
+	'use strict';
+
+	angular
+		.module('myApp')
+		.factory('JSONData', JSONData);
+
+	JSONData.$inject = ['$http'];
+
+	function JSONData($http) {
+		/**
+		 * Promise response function - success
+		 * Checks typeof data returned and succeeds if JS object, throws error if not
+		 *
+		 * @param response {*} data from $http
+		 * @returns {object|Array}
+		 * @private
+		 */
+		function _successRes(response) {
+			if (typeof response.data === 'object') {
+				return response.data;
+			} else {
+				throw new Error('retrieved data is not typeof object.');
+			}
+		}
+
+		/**
+		 * Promise response function - error
+		 * Throws an error with error data
+		 *
+		 * @param error {object}
+		 * @private
+		 */
+		function _errorRes(error) {
+			throw new Error('error retrieving data', error);
+		}
+
+		/**
+		 * GET local JSON data file and return results
+		 *
+		 * @returns {promise}
+		 */
+		function getLocalData() {
+			return $http
+				.get('/ng-app/data/data.json')
+				.then(_successRes, _errorRes);
+		}
+
+		// callable members
+		return {
+			getLocalData: getLocalData
+		}
+	}
+})();
