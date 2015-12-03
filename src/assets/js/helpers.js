fbab5cb84f6dae782b612fcf5298d0c0d1303e19
Kim Maida <yi.mihi@gmail.com>
2015-12-03 10:06:11 -0500

    FIX issue in scripts

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -22,13 +22,13 @@ window.helpers = (function() {
 	function fixBrowsers() {
 		var ua = navigator.userAgent.toLowerCase();
 		var chrome = ua.lastIndexOf('chrome/') > 0;
-		//var chromeversion = null;
+		var chromeversion = null;
 		var $html = $('html');
 		
 		// Modernizr 2 bug: Chrome on Windows 8 gives a false negative for transforms3d support
 		// Google does not plan to fix this; https://code.google.com/p/chromium/issues/detail?id=129004
 		if (chrome) {
-			var chromeversion = ua.substr(ua.lastIndexOf('chrome/') + 7, 2);
+			chromeversion = ua.substr(ua.lastIndexOf('chrome/') + 7, 2);
 			if (chromeversion >= 12 && $html.hasClass('no-csstransforms3d')) {
 				$html
 					.removeClass('no-csstransforms3d')
